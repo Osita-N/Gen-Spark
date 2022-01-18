4f3604d6fa115d8c9b403abeb4a9257818d5a87d
OsitaNduka <N4nduka@gmail.com>
2022-01-18 03:12:01 -0500

    Guess The Number
    Section 2 Project

---
+++ Guess The Number/src/GuessTheNumber.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+public class GuessTheNumber {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		// Scanning for input
+		Scanner input = new Scanner(System.in);
+		
+		
+		
+		// Random number chosen by computer
+		int r  = 0;
+		
+		// Answer given by user
+		int a = 0;
+
+		// Number of tries
+		int t = 6;
+		
+		System.out.print("Hello! What is your name?\n\n\n\n");
+		String one = input.nextLine();
+		
+		System.out.print("\n\n\n\n"+"Well, "+one+", I am thinking of a number between 1 and 20.\n\n"
+				+ "Take a guess.\n\n\n\n");
+		String two = input.nextLine();
+		
+		
+		
+		
+		
+		
+		
+	}// End Main
+
+}// End Class
