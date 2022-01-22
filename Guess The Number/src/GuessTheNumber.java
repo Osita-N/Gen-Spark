afc0d4a728f0416fbea07e04c33423dc463a8105
OsitaNduka <N4nduka@gmail.com>
2022-01-22 14:27:50 -0500

    Final
    Changed the higher and lower if statements. Also added a Exit function. Adjusted the (t) variable for tracking tries.

--- Guess The Number/src/GuessTheNumber.java
+++ Guess The Number/src/GuessTheNumber.java
@@ -20,16 +20,16 @@ public class GuessTheNumber {
 		int a = 0;
 
 		// Number of tries
-		int t = 6;
+		int t = 1;
 		
 		// Takes in the user Name
-		System.out.print("Hello! What is your name?\n\n\n\n");
+		System.out.print("Hello! What is your name?\n\n\n\n\n");
 		String name = input.nextLine();
 		
 		// Take in the users first guess
 		while (a <= 1 || a >= 20) {
 			
-			System.out.print("\n\n\n\n"+"Well, "+ name +", I am thinking of a number between 1 and 20.\n\n"
+			System.out.println("\n\n\n\n"+"Well, "+ name +", I am thinking of a number between 1 and 20.\n\n"
 					+ "Take a guess.\n\n\n\n");
 			String ans = input.nextLine();
 			
@@ -47,7 +47,7 @@ public class GuessTheNumber {
 		while((a < r || a > r) && t > 0) {
 			
 			// When guess is too low
-			if (a < r) {
+			while (a < r) {
 				
 				System.out.println("\n\n\n\nYour guess is too low\n\n"+"Take a guess.\n\n\n\n");
 				String ans = input.nextLine();
@@ -55,7 +55,7 @@ public class GuessTheNumber {
 				try {
 					
 					a = Integer.valueOf(ans);
-					t--;
+					t++;
 					//System.out.println("this is try count: "+t);
 					
 				}catch(NumberFormatException e) {
@@ -64,7 +64,7 @@ public class GuessTheNumber {
 			}//.if
 			
 			// When guess is to high
-			if (a > r) {
+			while (a > r) {
 				
 				System.out.println("\n\n\n\nYour guess is too high\n\n"+"Take a guess.\n\n\n\n");
 				String ans = input.nextLine();
@@ -72,8 +72,7 @@ public class GuessTheNumber {
 				try {
 					
 					a = Integer.valueOf(ans);
-					t--;
-					//System.out.println("this is try count: "+t);
+					t++;
 					
 				}catch(NumberFormatException e) {
 					
@@ -85,14 +84,18 @@ public class GuessTheNumber {
 				
 				System.out.println("\n\n\n\nGood job, "+name+"! You guessed my number in "+t+" guessess!\n\n"
 									+"Would you like to play again? (y or n)\n\n\n\n");
+				String ans = input.nextLine();
 								
 			}//.if
 			
 			// When the max amounts of tries are made
-			if (t == 0) {
+			if (t == 6) {
 				System.out.println("Better luck next time.\n\n"
 									+"you ran out of your "+t+" tries");
 			}
+			
+			// Exits the program
+			System.exit(0);
 			
 		}//.while
 		
